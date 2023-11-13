b2bf628270ac366870aa51c6822ada9a8c42c38f
Nikolas <timofeevnik41@gmail.com>
2023-11-14 00:10:09 +0400

    feat: deploy

---
+++ routers/__init__.py
@@ -0,0 +1,3 @@
+from .users import router as users_router
+from .locations import router as locations_router
+from .boardgames import router as boardgames_router
--- routers/boardgames.py
+++ routers/boardgames.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from pydantic import Field
 
 from utils.dependencies import CurrentUser
@@ -20,6 +20,6 @@ async def get_boardgame_by_id(bg_id: int):
 async def make_a_booking(
     user: CurrentUser,
     bg_id: int,
-    slot_id: int = Field(ge=1, le=24, description='Номер слота от 1 до 24 (слот = 1 час времени)')
+    slot_id: int = Body(..., ge=1, le=24, description='Номер слота от 1 до 24 (слот = 1 час времени)')
 ):
     """Создать заявку на бронирование настольной игры."""
